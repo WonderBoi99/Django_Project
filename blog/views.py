297e063cec64b5beec06179d54d0a8482ea543cd
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-16 15:11:06 -0700

    learned about templates, how to use static files like css, and godd code practises for using url instead of hardcoding

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Billy',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'data_posted': 'August 27,2023'
+    },
+    {
+        'author': 'John',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'data_posted': 'December 27,2023'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
